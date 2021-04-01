e6502fbe871628ea9b5b5d5bf83515a8f07ed367
unknown <deekshiavru@gmail.com>
2021-04-01 12:02:37 +0530

    VisualStudio

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using CustomerManagementProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerManagementProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private IRepo<Customer> _repo;
+        public CustomerController(IRepo<Customer> repo)
+        {
+            _repo = repo;
+        }
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return Ok(_repo.GetItems());
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody]Customer customer)
+        {
+            _repo.AddItems(customer);
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/CustomerRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementProject.Models
+{
+    public class CustomerRepo : IRepo<Customer>
+    {
+        public List<Customer> customers { get; set; }
+        public CustomerRepo()
+        {
+            customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    Id=1,
+                    Name="Hello",
+                    Age=120
+                }
+            };
+        }
+        public void AddItems(Customer item)
+        {
+            this.customers.Add(item);
+        }
+
+        public IEnumerable<Customer> GetItems()
+        {
+            return customers;
+        }
+    }
+}
